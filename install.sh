54aaa74c64819be096e0e1756a52fafa376aaba8
Crabmandable <zach.gerstman@gmail.com>
2021-01-08 08:12:00 -0500

    Install script

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure?} [Y/n] " response
+    case "$response" in
+        [nN][oO]|[nN]) 
+            false
+            ;;
+        *)
+            true
+            ;;
+    esac
+}
+
+# VIM
+source_vim="source $PWD/.vimrc \" Make sure this stays on line 1"
+if [ "$(head -n 1 ~/.vimrc)" == "$source_vim" ]; then
+    echo "The .vimrc is already installed"
+else
+    confirm "Would you like to install the .vimrc?" && sed -i "1i $source_vim" ~/.vimrc
+fi
+confirm "Would you like to manually edit the .vimrc now?" && vim ~/.vimrc
+confirm "Would you like to install vim plugins now?" && vim +PluginInstall +PluginClean! +qall
+
+echo "Finished"
